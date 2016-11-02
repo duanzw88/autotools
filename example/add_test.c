0bb739d331993f3d39909b4880e9bdd5429d314d
duanzw <duanzhongwei@jiachongs.com>
2016-11-02 17:58:42 +0800

    初步了解autotools工具，完成一个小demo

---
+++ example/add_test.c
@@ -0,0 +1,10 @@
+
+#include <stdio.h>
+#include "add.h"
+
+int main()
+{
+    printf("This is test_add\n");
+    printf("add(2,3) = %d\n",add(2,3));
+    return 0;
+}
---
+++ include/add.h
@@ -0,0 +1,6 @@
+
+#ifndef __ADD_H
+#define __ADD_H
+
+extern int add(int a,int b);
+#endif
